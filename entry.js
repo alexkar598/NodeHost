3224017190ac71fa4edc5eac946784a4f26b0f16
alexkar598 <>
2019-08-22 17:57:07 -0400

    CHANGES!!!

--- entry.js
+++ entry.js
@@ -1,22 +1,13 @@
-function string_to_charcodes (str) {
-	var retArray = []
-	for (var i = 0; i < str.length; i++) {
-		retArray.push(str.charCodeAt(i));
-	}
-	return retArray;
-}
-
-function buf2hex(buffer) { // buffer is an ArrayBuffer
+/* function buf2hex(buffer) { // buffer is an ArrayBuffer
 	return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-  }
+  } */
 
 const byond = require("byondlink")
 
 var link = new byond("localhost",5565,18595)
-link.send("?AAAAAAAA",(e) => {
+link.send("AAAAAAAA",(e) => {
 	console.log(e)
 })
-
-/*
-00 83 00 0f 00 00 00 00 00 3f 41 41 41 41 41 41 41 41 00
-*/
+link.addListener("topic",(topic) => {
+	console.log(topic)
+})
