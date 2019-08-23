08d805ab8f53ebdf915f0abe316b263fbe9a8e5a
alexkar598 <>
2019-08-23 01:01:42 -0400

    yeet broken code

--- entry.js
+++ entry.js
@@ -1,9 +1,21 @@
-/* function buf2hex(buffer) { // buffer is an ArrayBuffer
-	return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
-  } */
+/*
+const per = require("permit")
+const config = require("config")
 
+const express = require("express")
+const fs = require("fs")
+const https = require("https")
+const permit = new per.Basic()
+*/
 const byond = require("byondlink")
-
+/*
+const privateKey  = fs.readFileSync('ssl/server.key', 'utf8');
+const certificate = fs.readFileSync('ssl/server.crt', 'utf8');
+const credentials = {key: privateKey, cert: certificate};
+const app = express()
+const httpsServer = https.createServer(credentials,app)
+httpsServer.listen(443)
+*/
 var link = new byond("localhost",5565,18595)
 link.send("AAAAAAAA",(e) => {
 	console.log(e)
@@ -11,3 +23,16 @@ link.send("AAAAAAAA",(e) => {
 link.addListener("topic",(topic) => {
 	console.log(topic)
 })
+link.addListener("error",(err) => {
+	console.log(err.message)
+})
+
+
+
+
+
+/*API
+app.get(/./,(req,res) => {
+	res.status(200).end("<h1>Connection Successful</h1>")
+})
+*/
